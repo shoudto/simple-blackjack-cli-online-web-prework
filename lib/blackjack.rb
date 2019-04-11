245225070598bb22d184b716ee0acb6efbf632ed
Oscar Shoudt <shoudto@gmail.com>
2019-04-11 00:41:37 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 
 def end_game
   # code #end_game here
-  puts "Sorry, you hit #{display_card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{x}. Thanks for playing!"
 end
 
 def initial_round
