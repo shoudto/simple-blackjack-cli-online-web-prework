f958125579b70fa10b4c84a2d3cf05652524ed12
Oscar Shoudt <shoudto@gmail.com>
2019-04-11 02:13:35 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,36 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(x)
   # code #display_card_total here
+  puts "Your cards add up to #{x}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+
 end
 
 def hit?
--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -2,6 +2,7 @@ describe "#welcome" do
   it "prints a welcome message to screen" do
     expect($stdout).to receive(:puts).with("Welcome to the Blackjack Table")
     welcome
+    def square
   end
 end
 
